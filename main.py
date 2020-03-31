d93f8a436b20494f10db65e804647a82945ea703
DMH2021 <43209074+DMH2021@users.noreply.github.com>
2020-03-30 22:23:46 -0400

    Added Dynamic Stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/index.html')
@@ -15,4 +15,22 @@ def about_me():
 
 @app.route('/locations.html')
 def locations():
-    return render_template("locations.jinja2html")
+    return render_template("locations.jinja2")
+
+@app.route('/custom.html', method = ['POST'])
+def custom():
+    fname = request.form['fname']
+    lname = request.form['lname']
+    thing = request.form['favorite_thing']
+    name = fname + " " +lname
+    if thing == "BMC":
+        image_paths = 'static/BMC.jpg'
+    elif thing == "Cervelo":
+        image_paths = 'static/Cervelo.jpg'
+    elif thing == "Trek":
+        image_paths = 'static/Trek.jpg'
+    elif thing == "Canyon":
+        image_paths = 'static/Canyon.jpg'
+    elif thing == "Pinarello":
+        image_paths = 'static/Pinarello.jpg'
+    return render_template("custom.jinja2", full_name = name, image_paths = image_paths)
