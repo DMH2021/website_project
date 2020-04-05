7ee2e9ea0c0df7df2f1724bd626ac48af5e550aa
DMH2021 <43209074+DMH2021@users.noreply.github.com>
2020-04-04 23:58:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
-@app.route('/index.html')
+@app.route('/index.jinja2')
 def index():
     return render_template("index.jinja2")
 
-@app.route('/bike.html')
+@app.route('/bike.jinja2')
 def bike():
     return render_template("bike.jinja2")
 
-@app.route('/about_me.html')
+@app.route('/about_me.jinja2')
 def about_me():
     return render_template("about_me.jinja2")
 
-@app.route('/locations.html')
+@app.route('/locations.jinja2')
 def locations():
     return render_template("locations.jinja2")
 
-@app.route('/custom.html', methods = ['POST'])
+@app.route('/custom.jinja2', methods = ['POST'])
 def custom():
     fname = request.form['fname']
     lname = request.form['lname']
